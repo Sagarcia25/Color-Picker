35a63dcd90941ac9a3b97384ec7fbc72f721236c
Salvador <Sagarcia2713@gmail.com>
2022-01-12 15:29:01 -0600

    Duplicated the switch 2 times to have 3 total switches. Switches are connected to the ViewController swift file via both IBActions and IBOutlets. The outlets will give access to the isOn property. The property will then be used in the action to determine the color of the UIView. A new function was implemented to handle the logic of what color to display. Each switch handles a value or red, green, or blue. Depending on the logic, a color constant is made in the function to assign the background color of the UIView. This function named updateColor is called in the viewDidLoad and the IBAction.

--- Color Picker/ViewController.swift
+++ Color Picker/ViewController.swift
@@ -9,19 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redSwitch: UISwitch!
+    @IBOutlet var greenSwitch: UISwitch!
+    @IBOutlet var blueSwitch: UISwitch!
+    
     @IBOutlet var colorView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        colorView.backgroundColor = .black
+        updateColor()
     }
 
     @IBAction func switchChanged(_ sender: UISwitch) {
         
-        if sender.isOn{
-            colorView.backgroundColor = .red
-        } else {
-            colorView.backgroundColor = .black
+        updateColor()
+    }
+    
+    func updateColor(){
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        
+        if redSwitch.isOn{
+            red = 1
         }
+        if greenSwitch.isOn{
+            green = 1
+        }
+        if blueSwitch.isOn{
+            blue = 1
+        }
+        
+        let color = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        
+        colorView.backgroundColor = color
     }
     
 }
